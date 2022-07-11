bcdf922a51882c7aa30a64036b1705abcc8e7c8a
Fernando Corrêa <paracorrea@gmail.com>
2022-07-11 18:40:43 -0300

    Banco de dados e Tabela Criadas

---
+++ src/main/java/com/fernando/aulaspring/dominio/Categorias.java
@@ -0,0 +1,80 @@
+
+package com.fernando.aulaspring.dominio;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categorias implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer id;
+	String nome;
+	
+	
+	// Construtores	
+	public Categorias() {
+		
+	}
+	
+		public Categorias(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+		
+		
+	
+		
+	// métodos getters and setters
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Categorias [id=" + id + ", nome=" + nome + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categorias other = (Categorias) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+
+}
--- src/main/java/com/fernando/aulaspring/resources/CatagoriaResources.java
+++ src/main/java/com/fernando/aulaspring/resources/CatagoriaResources.java
@@ -1,17 +1,30 @@
 package com.fernando.aulaspring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fernando.aulaspring.dominio.Categorias;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CatagoriaResources {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List<Categorias> listar() {
 		
-		return "Rest funcionando...";
+		Categorias cat1 = new Categorias(1,"Informatica");
+		Categorias cat2 = new Categorias(2, "Escritório");
+		
+		List<Categorias> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		return lista;
 		
 	}
 	
