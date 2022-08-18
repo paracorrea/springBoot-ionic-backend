3be4f02b220b1077070ca6abc213fbcab66fc73a
Fernando Correa <paracorrea@gmail.com>
2022-08-18 07:10:49 -0300

    Classe endereco salva

--- src/main/java/com/fernando/aulaspring/dominio/Endereco.java
+++ src/main/java/com/fernando/aulaspring/dominio/Endereco.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Endereco implements Serializable{
@@ -22,6 +23,7 @@ public class Endereco implements Serializable{
 	private String bairro;
 	private String cep;
 	
+	@ManyToOne
 	private Cliente cliente;
 
 	public Endereco() {
