67101e63b0e6b3f26220140994761fd566e71112
Fernando Correa <paracorrea@gmail.com>
2022-08-22 20:22:43 -0300

    Pedido, Pagamento, Pagamento com Cartao, pagamento com boleto

--- src/main/java/com/fernando/aulaspring/dominio/Cliente.java
+++ src/main/java/com/fernando/aulaspring/dominio/Cliente.java
@@ -38,6 +38,9 @@ public class Cliente implements Serializable{
 	@ElementCollection
 	@CollectionTable(name="telefone")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
 		
@@ -94,6 +97,14 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -109,7 +120,7 @@ public class Cliente implements Serializable{
 		Cliente other = (Cliente) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 	
 
 }
